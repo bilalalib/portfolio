3c9787bd666d5d4d7c8406ed25c7033e0677d8af
Bilal Ahmad <biahmad.bese24seecs@seecs.edu.pk>
2026-05-26 18:21:14 +0500

    fixed mobile text wrapping

--- src/commands/banner.ts
+++ src/commands/banner.ts
@@ -21,8 +21,8 @@ const createBanner = () : string[] => {
   banner.push("Type <span class='command'>'about'</span> to read my bio.");
   banner.push("Type <span class='command'>'projects'</span> to view my latest work.");
   banner.push("Type <span class='command'>'help'</span> for a list of all available commands.");
-  banner.push(`Type <span class='command'>'repo'</span> to view the GitHub repository or click <a href='${command.repoLink}' target='_blank'>here</a>.`);
-  banner.push("Type <span class='command'>'theme light'</span> and <span class='command'>'theme dark'</span> to switch between light and dark mode.");
+  banner.push(`<span class='responsive-text'>Type <span class='command'>'repo'</span> to view the GitHub repository or click <a href='${command.repoLink}' target='_blank'>here</a>.</span>`);
+  banner.push("<span class='responsive-text'>Type <span class='command'>'theme light'</span> and <span class='command'>'theme dark'</span> to switch between light and dark themes.</span>");
   banner.push("<br>");
   return banner;
 }
